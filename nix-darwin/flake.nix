b322653e864cff2e38da2165bdaf79e91220a2d3
Eirik Vågeskar <eirikvageskar@Eirik-sin-MacBook-Pro.local>
2024-03-04 15:19:36 +0100

    nix-darwin input reorganized

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
