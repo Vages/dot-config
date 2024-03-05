0e3ac9b46a1a04ce75e1308b77d673d5bb9c9533
Eirik Vågeskar <eirik.vaageskar@aboveit.no>
2024-03-05 08:55:11 +0100

    Delta used as pager for Git

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -128,7 +128,7 @@
           # for home-manager, don't change this!
           stateVersion = "23.05";
 
-          packages = with pkgs; [ nixpkgs-fmt ];
+          packages = with pkgs; [ nixpkgs-fmt delta ];
           sessionVariables = {
             EDITOR = "subl -w";
           };
@@ -159,6 +159,16 @@
             extraConfig = {
               init.defaultBranch = "main";
               push.autoSetupRemote = true;
+
+              # Taken from https://github.com/dandavison/delta?tab=readme-ov-file#get-started
+              core.pager = "delta";
+              interactive.diffFilter = "delta --color-only";
+              delta = {
+                navigate = true; # use n and N to move between diff sections
+                light = true; # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
+              };
+              merge.conflictstyle = "diff3";
+              diff.colorMoved = "default";
             };
           };
         };
