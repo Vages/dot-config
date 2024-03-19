fecc8ef2db0052cce9a24cda916581d9146a0839
Eirik Vågeskar <eirik.vaageskar@aboveit.no>
2024-03-19 08:14:40 +0100

    TextSniper added to masApps

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -129,6 +129,7 @@
           ];
           masApps = {
             "1Password for Safari" = 1569813296;
+            "TextSniper" = 1528890965;
           };
           onActivation.cleanup = "uninstall";
         };
