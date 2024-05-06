932f724d03b8985c784d55b88e23e4e4debd871a
Eirik Vågeskar <eirik.vaageskar@aboveit.no>
2024-05-06 12:53:19 +0200

    nix-darwin URL changed back to official repo
    Stop using the fork that was introduced in 59217d1.

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:wegank/nix-darwin/mddoc-remove";
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
